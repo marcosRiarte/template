f8a1daa32841f71d980a6e4caaae510dd6ff6f65
marcosRiarte <mriarte@fi.uba.ar>
2016-09-01 18:45:23 -0300

    Version Final

--- src/test/java/ar/fiuba/tdd/template/QueueTest.java
+++ src/test/java/ar/fiuba/tdd/template/QueueTest.java
@@ -1,47 +1,45 @@
-package ar.fiuba.tdd.template;
-
-public class QueueTest extends TestCase {
-        Queue cola = new Queue();
-        int number = 8;
-        String string ="seven";
-@Test
-public void ListEmpty() {
+public class QueueTest<T> extends TestCase {
+    private Queue <T>cola = new Queue<T>();
+    private T number;
+    private T string;
+    @Test
+    public void ListEmpty() {
         System.out.println("Test of List Empty");
         assertTrue(this.cola.isEmpty());
-        }
+    }
 
-@Test
-public void seeSize(){
+    @Test
+    public void seeSize(){
         System.out.println("Test of size of the queue");
         assertEquals("Result with size = 0  value is : ",0,this.cola.size());
         this.cola.add(this.number);
         assertEquals("Result with size = 1 value is : ",1,this.cola.size());
         this.cola.add(this.string);
         assertEquals("Result with size = 2 value is : ",2,this.cola.size());
-        }
+    }
 
-@Test
-public void addElement() {
+    @Test
+    public void addElement() {
         System.out.println("Test add a element to queued");
         assertEquals("Result with size = 0 value is : ",0,this.cola.size());
-        int number2= 9;
+        T number2 = (T) new Integer(2);
         this.cola.add(number2);
         assertEquals("Result with size = 1 value is : ",1,this.cola.size());
 
-        }
+    }
 
-
-@Test
+    @Test
     public void firstElementBack() {
         System.out.println("Test of take the first queue element");
-        assertEquals("The first element is  : ",8,this.cola.top());
+        assertTrue (cola.top() != null );
 
     }
 
-@Test
+    @Test
     public void removeElement(){
         System.out.println("Test to remove the first element of the queue");
+        int tamañoActual=this.cola.size();
         this.cola.remove();
-        assertTrue(cola.top()!=8);
+        assertTrue(cola.size()== (tamañoActual -1));
     }
 }
