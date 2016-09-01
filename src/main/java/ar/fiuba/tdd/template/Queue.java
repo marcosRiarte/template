a369ed4c754482375458f72daf6df64f593f99a5
marcosRiarte <mriarte@fi.uba.ar>
2016-09-01 18:44:30 -0300

    Version Final

--- src/main/java/ar/fiuba/tdd/template/Queue.java
+++ src/main/java/ar/fiuba/tdd/template/Queue.java
@@ -1,4 +1,4 @@
-public class Queue<T> implements Queueable {
+public class Queue<T> implements Queueable<T>{
     class Node {
 
         private T element;
@@ -12,25 +12,17 @@ public class Queue<T> implements Queueable {
     }
 
     private Node first;
-    private Node end;
+    private Node end = null;
     private int size = 0;
 
 
-    public Queue() {
-        this.end = null;
-    }
-    public boolean isEmpty() {
-        return (size == 0);
-    }
-    public int size() {
-        return this.size;
-    }
+    public boolean isEmpty() { return (size == 0); }
+    public int size() { return this.size; }
     public void add(T item) {
         Node new_node = new Node(item);
         if (this.end == null) {
             this.end = new_node;
         } else {
-
             new_node.next = this.end;
             this.end = new_node;
 
@@ -38,26 +30,21 @@ public class Queue<T> implements Queueable {
         size++;
     }
 
-//public T top(){try{return this.first.element;}throw new AssertionError();} ;
-
-    //public T top(){ assert this.size() > 1 : "ERROR";return top2();};
     public T top() {
-
-        if (this.first == null)
-            return null;
+        if (this.size == 0)
+            throw new AssertionError();
 
         else
-            return this.first.elem;
+            return this.first.element;
 
     }
 
     public void remove() {
-        if (this.first == null)
-            return null;
-        T generic = this.first.elem;
-        this.first = this.first.next;
+        if (this.size == 0)
+            throw new AssertionError();
+        else
+            this.first = this.first.next;
         size--;
-        return generic;
 
     }
 }
--- src/main/java/ar/fiuba/tdd/template/Queueable.java
+++ src/main/java/ar/fiuba/tdd/template/Queueable.java
@@ -1,12 +1,8 @@
-public interface Queueable {
+public interface Queueable <T>{
     boolean isEmpty();
     int size();
-    void add(T item); //Agregar un item.
-    T top(); //retornar el primer item,lanzar exception si esta vacío.
-    void remove(); //remover el primer item ,lanzar exception si esta vacío.
- }
-    Once you're signed in, and we've initially synchronized your repositories from GitHub, go to your profile page for open source or for your private projects.
+    void add(T item); // agregar un item
+    T top(); // retornar el primer item, lanzar exception si esta vacío.
+    void remove(); // remover el primer item,lanzar exception si esta vacío.
 
-        You'll see all the organizations you're a member of and all the repositories you have access to. The ones you have administrative access to are the ones you can enable the service hook for.
-
-        Flip the switch to on for all repositories you'd like to enable.
+}
