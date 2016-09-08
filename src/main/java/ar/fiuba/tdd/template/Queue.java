9efd320437b5c6909196bbd73196e65a0fea888c
marcosRiarte <mriarte@fi.uba.ar>
2016-09-08 15:53:29 -0300

    verion17

--- src/main/java/ar/fiuba/tdd/template/Queue.java
+++ src/main/java/ar/fiuba/tdd/template/Queue.java
@@ -26,37 +26,26 @@ public class Queue<T> implements Queueable<T> {
 
     public void add(T item) {
         Node node = new Node(item);
-        if (this.end == null) {
+        if (this.end == null)
             this.end = node;
-
-        }
-        else {
+        else
             node.next = this.end;
             this.end = node;
-        }
         size++;
     }
 
     public T top() {
-        if (this.size == 0) {
+        if (this.size == 0)
             throw new AssertionError("Lista vacia");
-        }
-        else {
+        else
             return this.first.element;
-        }
     }
 
     public void remove() {
-        if (this.size == 0) {
+        if (this.size == 0)
             throw new java.lang.AssertionError("Lista vacia");
-
-        }
-
-        else {
+        else
             this.first = this.first.next;
-
-        }
         size--;
-
     }
 }
