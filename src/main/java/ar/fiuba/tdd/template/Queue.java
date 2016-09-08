6ef498f53f183a19bdcd9ce4b9e2a93d842f91ab
marcosRiarte <mriarte@fi.uba.ar>
2016-09-08 16:12:24 -0300

    A

--- src/main/java/ar/fiuba/tdd/template/Queue.java
+++ src/main/java/ar/fiuba/tdd/template/Queue.java
@@ -26,7 +26,7 @@ public class Queue<T> implements Queueable<T> {
 
     public void add(T item) {
         Node node = new Node(item);
-        if (this.end == null) this.end = node;
+        if (this.end == null) {this.end = node };
         else {
             node.next = this.end;
             this.end = node;
